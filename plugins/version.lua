683c2d30ec24b839808c90c4cefee718cc22c035
Death15 <arash.official15@gmail.com>
2016-02-19 14:31:56 -0800

    Update version.lua

--- plugins/version.lua
+++ plugins/version.lua
@@ -1,16 +1,20 @@
 do
 
 function run(msg, matches)
-  return 'Telegram Bot '.. VERSION .. [[ 
-  Checkout http://git.io/6jdjGg
-  GNU GPL v2 license.]]
+  return 'SuperSwatch version 3.0  [[ 
+  For More Info : @ArashSwatch
+  Creator : @ArashSwatch
+  Our Channel : @SWATCHTEAM
+  -----------------------------
+  Our Support : @SwatchSupport
+  GNU GPL v3 license.]]
 end
 
 return {
   description = "Shows bot version", 
-  usage = "!version: Shows bot version",
+  usage = "/version: Shows bot version",
   patterns = {
-    "^!version$"
+    "^[!/](version)$"
   }, 
   run = run 
 }
